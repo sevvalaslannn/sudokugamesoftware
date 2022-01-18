15f360b8feebd5b365492418c9d2b53c327ade05
sevval <sevval.aslan@alumni.mondragon.edu>
2022-01-18 23:48:29 +0100

    all games  mocked

--- src/main/java/sudokugames/MenuController.java
+++ src/main/java/sudokugames/MenuController.java
@@ -26,39 +26,39 @@ public class MenuController implements ActionListener {
                 break;
             case "game2":
                 System.out.println("IS  PRESSING GAME 2");
-                vista.changeGamePanel(new GameTwo(vista));
+                vista.changeGamePanel(new GameTwo(vista,null));
                 break;
             case "game3":
                 System.out.println("IS  PRESSING GAME 3");
-                vista.changeGamePanel(new GameThree(vista));
+                vista.changeGamePanel(new GameThree(vista,null));
                 break;
             case "game4":
                 System.out.println("IS  PRESSING GAME 4");
-                vista.changeGamePanel(new GameFour(vista));
+                vista.changeGamePanel(new GameFour(vista,null));
                 break;
             case "game5":
                 System.out.println("IS  PRESSING GAME 5");
-                vista.changeGamePanel(new GameFive(vista));
+                vista.changeGamePanel(new GameFive(vista,null));
                 break;
             case "game6":
                 System.out.println("IS  PRESSING GAME 6");
-                vista.changeGamePanel(new GameSix(vista));
+                vista.changeGamePanel(new GameSix(vista,null));
                 break;
             case "game7":
                 System.out.println("IS  PRESSING GAME 7");
-                vista.changeGamePanel(new GameSeven(vista));
+                vista.changeGamePanel(new GameSeven(vista,null));
                 break;
             case "game8":
                 System.out.println("IS  PRESSING GAME 8");
-                vista.changeGamePanel(new GameEight(vista));
+                vista.changeGamePanel(new GameEight(vista,null));
                 break;
             case "game9":
                 System.out.println("IS  PRESSING GAME 9");
-                vista.changeGamePanel(new GameNine(vista));
+                vista.changeGamePanel(new GameNine(vista,null));
                 break;
             case "game10":
                 System.out.println("IS  PRESSING GAME 10");
-                vista.changeGamePanel(new GameTen(vista));
+                vista.changeGamePanel(new GameTen(vista,null));
                 break;
         }
     }
